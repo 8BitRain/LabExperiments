f54b213b96d52336173ab53b34c79190494a1843
Eric <8BitRain@gmail.com>
2022-04-24 16:15:35 -0500

    Adding logic to allow a gameobject to shadow its parents animations

---
+++ Animation/FollowAnimations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAnimations : MonoBehaviour
+{
+    private Animator animator;
+    private AnimationController parentAnimationController;
+    private AnimationController animationController;
+
+    private string currentAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        parentAnimationController = this.transform.GetComponentInParent<AnimationController>();
+        animationController = this.GetComponent<AnimationController>(); 
+    }
+
+    // Shadow parent animation
+    // Note, this system may not work with bools
+    void Update()
+    {
+        if(currentAnimation != parentAnimationController.GetCurrentState())
+        {
+            Debug.Log("currentAnimation");
+            currentAnimation = parentAnimationController.GetCurrentState();
+            animationController.ChangeAnimationState(animator, currentAnimation);
+        }
+    }
+}

e65e159312877e91363622dce7a589d8b1b4a276
Eric <8BitRain@gmail.com>
2022-05-26 08:49:17 -0500

    Updated AI HUD to use stamina bar

--- UI/NPCHUD.cs
+++ UI/NPCHUD.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class NPCHUD : MonoBehaviour
 {
     public GameObject HealthBar;
+    public GameObject StaminaBar;
     private LabGameManager gameManager;
 
     private GameObject healthBarInstance;
+    private GameObject staminaBarInstance;
 
     private void OnEnable()
     {
         Debug.Log("enabled");
         Debug.Log(EventsManager.instance);
         CameraController.onCameraLoaded += SpawnHealthBar;
+        CameraController.onCameraLoaded += SpawnStaminaBar;
     }
 
     private void OnDisable()
     {
         CameraController.onCameraLoaded -= SpawnHealthBar;
+        CameraController.onCameraLoaded -= SpawnStaminaBar;
     }
 
     private void OnDestroy()
     {
        CameraController.onCameraLoaded -= SpawnHealthBar;
+       CameraController.onCameraLoaded -= SpawnStaminaBar;
     }
 
     public void Start()
@@ -33,7 +38,7 @@ public class NPCHUD : MonoBehaviour
 
     public void SpawnHealthBar(Camera cameraInstance)
     {
-        Debug.Log("Happens");
+        Debug.Log("Healthbar for " + this.gameObject.name + " spawned!");
         healthBarInstance = Instantiate(HealthBar, this.transform.position, this.transform.rotation);
         healthBarInstance.transform.SetParent(this.transform);
         
@@ -56,8 +61,52 @@ public class NPCHUD : MonoBehaviour
             
         }
 
+        
+
         //UIFaceCamera is a script that is attached to the GameObject that is a child of the HealthBarCanvas    
         if(healthBarInstance.transform.GetChild(0).TryGetComponent<UIFaceCamera>(out UIFaceCamera UIComponent))
+        {
+            UIComponent.mainCamera = cameraInstance;
+            if(TryGetComponent<Body>(out Body bodyComponent))
+            {
+                UIComponent.SetTrackingObject(bodyComponent.Head.gameObject);
+            }
+            else
+            {
+                UIComponent.SetTrackingObject(this.gameObject);
+            }
+        } 
+    }
+
+    public void SpawnStaminaBar(Camera cameraInstance)
+    {
+        Debug.Log("Stamina Bar for " + this.gameObject.name + " spawned!");
+        staminaBarInstance = Instantiate(StaminaBar, this.transform.position, this.transform.rotation);
+        staminaBarInstance.transform.SetParent(this.transform);
+        
+        //HealthBar is a scrip that is attached to the GameObject that is a child of the HealthBarCanvas
+        if(staminaBarInstance.transform.GetChild(0).TryGetComponent<StaminaBar>(out StaminaBar staminaBar))
+        {
+            Debug.Log("Setting owner instance to: " + this.gameObject.name);
+            staminaBar.SetOwnerInstance(this.gameObject);
+
+            //Set Healthbar max health
+            if(TryGetComponent<Status>(out Status status))
+            {
+                staminaBar.SetMaxStamina(status.stamina);
+                staminaBar.SetStamina(status.stamina);
+            }
+            else
+            {
+                Debug.Log("No Status script attached to: " + this.gameObject.name);
+            }
+            
+        }
+
+        
+
+        //UIFaceCamera is a script that is attached to the GameObject that is a child of the HealthBarCanvas    
+        if(staminaBarInstance.transform.GetChild(0).TryGetComponent<UIFaceCamera>(out UIFaceCamera UIComponent))
         {
             UIComponent.mainCamera = cameraInstance;
             if(TryGetComponent<Body>(out Body bodyComponent))

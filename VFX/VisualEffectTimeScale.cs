8d73e3324c4006a95c21dae8e02b0d6e34a04624
Eric <8BitRain@gmail.com>
2022-07-01 08:19:32 -0500

    Bugfix, vfx time scale script was not properly assigning vfx game object

--- VFX/VisualEffectTimeScale.cs
+++ VFX/VisualEffectTimeScale.cs
@@ -5,19 +5,23 @@ using UnityEngine.VFX;
 //https://realtimevfx.com/t/is-there-a-way-to-speed-up-slow-down-an-entire-vfx-graph-system-as-a-whole/10711/10
 [RequireComponent(typeof(VisualEffect))]
 [ExecuteInEditMode]
-public class VisualEffectTimeScale : MonoBehaviour
-{
+public class VisualEffectTimeScale : MonoBehaviour{
     [Range(0.0f, 10000.0f)] public float SimulationTimeScale = 1.0f;
 
-private VisualEffect Graph;
+    private VisualEffect Graph;
 
-private void OnValidate()
-{
-    Graph = gameObject.GetComponent<VisualEffect>();
-}
+    private void OnValidate()
+    {
+        Graph = gameObject.GetComponent<VisualEffect>();
+    }
 
-private void Update()
-{
-    Graph.playRate = SimulationTimeScale;
-}
+    private void Start()
+    {
+        Graph = gameObject.GetComponent<VisualEffect>();
+    }
+
+    private void Update()
+    {
+        Graph.playRate = SimulationTimeScale;
+    }
 }

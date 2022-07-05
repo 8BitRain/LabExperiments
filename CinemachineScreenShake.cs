f6b91ff4ba4bff582394dae5dc6c65c0781ce31a
Eric <8BitRain@gmail.com>
2022-07-04 21:37:28 -0500

    Updated screenshake logic to work with freelook cam and geenric virtual camera

--- CinemachineScreenShake.cs
+++ CinemachineScreenShake.cs
@@ -5,27 +5,53 @@ using Cinemachine;
 
 public class CinemachineScreenShake : MonoBehaviour
 {
-    public CinemachineFreeLook cinemachineCam;
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private CinemachineFreeLook cinemachineFreeLookCam;
     private float defaultFOV;
 
     public void Start()
     {
-        defaultFOV = cinemachineCam.m_Lens.FieldOfView;
+        //Attempt to grab thirdperson camera reference. If not available, grab Cinemachine Virtual Camera reference
+        if(TryGetComponent<CinemachineFreeLook>(out CinemachineFreeLook cinemachineFreeLookCam))
+        {
+            this.cinemachineFreeLookCam = cinemachineFreeLookCam;
+            defaultFOV = cinemachineFreeLookCam.m_Lens.FieldOfView;
+        }
+        else if(TryGetComponent<CinemachineVirtualCamera>(out CinemachineVirtualCamera cinemachineVirtualCamera))
+        {
+            this.cinemachineVirtualCamera = cinemachineVirtualCamera;
+            defaultFOV= cinemachineVirtualCamera.m_Lens.FieldOfView;
+        }
     }
 
     void Noise(float amplitude, float frequency)
     {
-        cinemachineCam.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
-        cinemachineCam.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
-        cinemachineCam.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
-        cinemachineCam.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
-        cinemachineCam.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
-        cinemachineCam.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+        if(cinemachineFreeLookCam != null)
+        {
+            cinemachineFreeLookCam.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
+            cinemachineFreeLookCam.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+            cinemachineFreeLookCam.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
+            cinemachineFreeLookCam.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+            cinemachineFreeLookCam.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
+            cinemachineFreeLookCam.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+        }
+        else if( cinemachineVirtualCamera != null)
+        {
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
+        }
     }
 
     void SetZoom(float zoom)
     {
-        cinemachineCam.m_Lens.FieldOfView = zoom;
+        if(cinemachineFreeLookCam != null)
+        {
+            cinemachineFreeLookCam.m_Lens.FieldOfView = zoom;
+        } 
+        else if (cinemachineVirtualCamera != null)
+        {
+            cinemachineVirtualCamera.m_Lens.FieldOfView = zoom;
+        }
     }
 
     public void DoShake(ScreenShakeComponent screenShakeComponent)

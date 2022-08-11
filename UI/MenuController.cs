b5435b68855e362ca086f05ba30dbb220c4d14ed
Eric <8BitRain@gmail.com>
2022-08-10 23:06:58 -0500

    Added logic to Open Menu

---
+++ UI/MenuController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class MenuController : MonoBehaviour
+{
+    private bool menuOpen = false;
+    public RectTransform menu;
+
+    public InputActionReference startInput;
+
+    void Update()
+    {
+        if(startInput.action.triggered && !menuOpen)
+        {
+            Debug.Log("Open Window");
+            OpenMenu();
+            return;
+        }
+        
+        if(startInput.action.triggered && menuOpen)
+        {
+            Debug.Log("Close Window");
+            CloseMenu();
+            return;
+        }
+    }
+
+    void OpenMenu()
+    {
+        if(menu != null)
+        {
+            menu.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+            menuOpen = true;
+        }
+    }
+
+    void CloseMenu()
+    {
+        if(menu != null)
+        {
+            menu.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+            menuOpen = false;
+        }
+    }
+    
+}

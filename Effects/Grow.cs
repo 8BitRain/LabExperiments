a5e3d6095fe1e568718edf18704f45606af9984d
Eric <8BitRain@gmail.com>
2022-08-21 10:32:49 -0500

    Added grow class for controlling ring shaped aoe AI attacks

---
+++ Effects/Grow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Grow : MonoBehaviour
+{
+    public float scaleX;
+    public float scaleY;
+    public float scaleZ;
+    public float scaleOverTime;
+    public bool destroyOnComplete = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.DOScale(new Vector3(scaleX, scaleY, scaleZ), scaleOverTime).OnComplete(() => {
+            if(destroyOnComplete)
+            {
+                transform.GetComponent<Renderer>().material.DOFade(0f, 5f);
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

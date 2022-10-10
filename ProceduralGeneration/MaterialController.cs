b698a883e861ee545148adaf9842d4b92e3046ef
Eric Smith <8BitRain@gmail.com>
2022-10-09 22:18:05 -0500

    Created MaterialController to manage materials on gameobjects

---
+++ ProceduralGeneration/MaterialController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MaterialController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] assets;
+    void Start()
+    {
+        DoFadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DoFadeIn()
+    {
+        foreach (GameObject asset in assets)
+        {
+            if(asset.TryGetComponent<Renderer>(out Renderer renderer))
+            {
+                try
+                {
+                    for (int i = 0; i < renderer.materials.Length; i++)
+                    {
+                        renderer.materials[i].DOFloat(1, "_OPACITY", 1f);    
+                    }
+                }
+                catch (System.Exception)
+                {
+                    Debug.LogError("Material Controller attempted to fade " + renderer.name + "  and failed >_<!");
+                }
+            }
+            else
+            {
+                Debug.Log("No renderer attached to " + asset.name);
+            }
+        }
+    }
+}

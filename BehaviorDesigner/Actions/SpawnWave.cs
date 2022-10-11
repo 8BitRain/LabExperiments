391d58d77506b0d85bb22665ee8a3e6493b4f7fa
Eric Smith <8BitRain@gmail.com>
2022-10-10 21:34:57 -0500

    Added additonal logic to spawn wave to assign behavior tree variables

---
+++ BehaviorDesigner/Actions/SpawnWave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class SpawnWave : Action
+{
+
+    public SharedGameObjectList spawnedWave;
+    private AIDirector AIDirector;
+
+
+    public override void OnStart()
+    {
+        AIDirector = transform.GetComponent<AIDirector>();
+        spawnedWave.Value = AIDirector.SpawnAIUnits(AIDirector.Waves[0]);
+
+    }
+    public override TaskStatus OnUpdate()
+    {
+
+        return TaskStatus.Success;
+    }
+}
--- Directors/AIDirector.cs
+++ Directors/AIDirector.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using BehaviorDesigner.Runtime;
 public class AIDirector : MonoBehaviour
 {
+    [Header("Wave Generator")]
     public Wave[] Waves;
     public Enemy[] AIEnemies;
 
-    public Transform spawnOrigin;
+    public Transform AIGroup;
 
-    ArrayList spawnedWave = new ArrayList();
+    public Transform spawnOrigin;
+    [Header("Behavior Tree Necessary Variables")]
+    public  List<GameObject> spawnedWave = new List<GameObject>();
+    public GameObject Player;
 
 
     private Dictionary<AIUnit.AIUnitType, Enemy> AIEnemyDictionary = new Dictionary<AIUnit.AIUnitType, Enemy>();
@@ -29,7 +33,7 @@ public class AIDirector : MonoBehaviour
         
     }
 
-    void SpawnAIUnits(Wave wave){
+    public List<GameObject> SpawnAIUnits(Wave wave){
         //Loop through the wave and spawn the AI unity from the Wave
         for (int i = 0; i < wave.AIUnits.Length; i++)
         {
@@ -38,9 +42,15 @@ public class AIDirector : MonoBehaviour
                 //Access the AIEnemyDictionary to spawn the correct AI unit based 
                 //on the information supplied by wave
                 Debug.Log("Load " + AIEnemyDictionary[wave.AIUnits[i].AIUnitEntity]);
-                Vector3 spawnOffset = new Vector3(0, Random.Range(0,10), Random.Range(0,10)) + spawnOrigin.position;
-                Instantiate(AIEnemyDictionary[wave.AIUnits[i].AIUnitEntity], spawnOffset, spawnOrigin.rotation);
+                Vector3 spawnOffset = new Vector3(0, 20, Random.Range(0,40)) + spawnOrigin.position;
+                GameObject AIUnit = Instantiate(AIEnemyDictionary[wave.AIUnits[i].AIUnitEntity], spawnOffset, spawnOrigin.rotation).gameObject;
+                AIUnit.transform.SetParent(AIGroup);
+
+                AIUnit.GetComponent<BehaviorTree>().SetVariableValue("TargetGameObject", Player);
+                
+                spawnedWave.Add(AIUnit);
             }
         }
+        return spawnedWave;
     }
 }
